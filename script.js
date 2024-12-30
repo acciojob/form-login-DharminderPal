1d97bcaec388cd71cab0502b45a1cbb22ac05cdb
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-30 10:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+   event.preventDefault();
+            const form = document.getElementById('form1');
+            const fname = form.elements['fname'].value;
+            const lname = form.elements['lname'].value;
+            alert(`${fname} ${lname}`);
 
 }
